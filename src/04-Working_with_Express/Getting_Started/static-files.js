41de8647c7f4c21cef6a19583cb80e4f1f1e5bcb
Karim A C <mail@ackarim.com>
2016-03-11 10:03:37 +0000

    Fixing current dir in static files

--- src/04-Working_with_Express/Getting_Started/static-files.js
+++ src/04-Working_with_Express/Getting_Started/static-files.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 //$ curl localhost:3000/public.txt
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 //$ curl localhost:3000/file.txt
-app.use(express.static('files'));
+app.use(express.static(__dirname + '/files'));
 
 //$ curl localhost:3000/static/public.txt
-app.use('/static', express.static('public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
