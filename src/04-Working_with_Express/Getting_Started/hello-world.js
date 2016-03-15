aa7e306e361e17e633e95bd19305d6d44c136146
Karim A C <mail@ackarim.com>
2016-03-15 11:16:48 +0000

    Changing static files for the main example

--- src/04-Working_with_Express/Getting_Started/hello-world.js
+++ src/04-Working_with_Express/Getting_Started/hello-world.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+// To test it open a terminal $ curl localhost:3000
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -8,5 +9,3 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-// To test it open a terminal $ curl localhost:3000
